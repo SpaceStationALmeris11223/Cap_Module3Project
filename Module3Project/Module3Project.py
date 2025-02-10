7a19f7163fc8353d656f0b5568a31c1be1bc15ff
SpaceStationALmeris11223 <125327160+SpaceStationALmeris11223@users.noreply.github.com>
2025-02-10 23:42:48 +0000

    v1

--- Module3Project/Module3Project.py
+++ Module3Project/Module3Project.py
@@ -2,15 +2,15 @@
 #Author A.M.H
 
 #Store our ice creams shop's menu items
-flavors = ["Vanilla", "Caramel", "Mint", "Cookie Dough"]
-toppings = ["sprinkles", "nuts", "cherry","strawberrys"]
+cones = ["waffle", "sugar", "plain","cake"]
+flavors = ["vanilla", "caramel", "mint", "cookie dough","pistachio", "neapolitan "]
+toppings = ["sprinkles", "nuts", "cherry","strawberries"]
 prices = {"Scoop":2.50, "Toppings": 0.50 }
 
 def display_menu ():
     """Shows avaiable flavors and toppings to the customer"""
     print("\n===welcome to the Icecream Shop! ===")
     print("\nAvailable flavors:")
-
     #Loop through the flavor list and show each flavor, then
     # we'll loop though the toppings list and display them
     for flavor in flavors:
@@ -23,10 +23,31 @@ def display_menu ():
     #Display Prices
     print("\nPrices")
     print(f"Scoops: ${prices['Scoop']:.2f} each") 
-    #2f means t digits the the left of the float(Price)
+    #2f means two digits the the left of the float(Price)
     print(f"Toppings: ${prices['Toppings']:.2f} each")
 
+def search_flavor():
+    while True:
+        flavor_search = input("\nEnter a flavor to see if it's available (or type 'done' to exit)  ").lower()
 
+        if flavor_search == "done":
+            break
+
+        if flavor_search in flavors:
+            print(f"Yep! {flavor_search.title()} is available. ")
+        else:
+            print(f"Sorry, {flavor_search.title()} is not available at this location. ")
+
+def get_cone():
+    """Gets the cone options for the customer"""
+    while True:
+        cone = input("\nWhat cone would you like to put your ice cream on?(cake, sugar, plain, waffle) ").lower()
+        if cone in cones:
+            print(f"added cone")
+            return cone
+        else:
+            print("Sorry that cone isn't available.")
+    
 def get_flavors():
     """Gets ice cream flavor choices from the customer"""
     chosen_flavors = []
@@ -34,22 +55,23 @@ def get_flavors():
     while True:
         try:
             #Prompt user to choose theri scoops of ice cream
-            num_scoops = int(input("\n how many scoops would you like? (1-4)"))#input always comes as a string
+            num_scoops = int(input("\n how many scoops would you like? (1-4) "))#input always comes as a string
             if 1 <= num_scoops <=4:
                 break
             print("Please choose between 1-4 scoops.")
         except ValueError:
             print("Please enter a number.")
+
     
     #Prompt the user to enter an ice cream flavor
-    print("\nFor each scoop, enter the flavor you'd like:")
+    print("\nFor each scoop, enter the flavor you'd like:(vanilla, cookie dough, mint, caramel, pistachio, neapolitan)")
     for i in range(num_scoops):# For loop prompts for each scoop of the ice cream
         #nested while loop handles user input and validation
         while True:
-            flavors = input(f"Scoop {i+1}: ").lower()
+            flavor = input(f"Scoop {i+1}: ").lower()
             #check to see if their chosen flavor is on the list
-            if flavors in flavors:
-                chosen_flavors.append(flavors)
+            if flavor in flavors:
+                chosen_flavors.append(flavor)
                 break
             print("Sorry, that flavor isn't available.")
 
@@ -62,13 +84,13 @@ def get_toppings():
     #use while loop to prompt user for their toppings until they are done
     #choosing 
     while True:
-        topping = input("\nEnter a topping( sprinkles, nuts, cherrys, strawberrys or done if finished): ").lower()
+        topping = input("\nEnter a topping( sprinkles, nuts, cherry, strawberries or done if finished): ").lower()
         if topping == "done":
             break
         #test if toppings are in shop
         if topping in toppings:
             chosen_toppings.append(topping)
-            print(f"added topping")
+            print(f"added topping!")
         else:
             print("Sorry that topping isn't available.")
     
@@ -76,38 +98,62 @@ def get_toppings():
 
 def calculate_total(num_scoops, num_toppings):
     """Calculates the total cost of the order"""
+    discounted = False
     scoop_cost = num_scoops * prices["Scoop"]
     toppings_cost = num_toppings * prices["Toppings"]
-    return scoop_cost + toppings_cost
+    total = scoop_cost + toppings_cost
+    if total >= 10.00:
+        total *= 0.9
+        discounted = True
+    return scoop_cost, toppings_cost, total, discounted
 
-def print_receipt(num_scoops, chosen_flavors, chosen_toppings):
+def print_receipt(num_scoops, chosen_flavors, chosen_toppings, chosen_cone):
     print("\n=== Your Ice cream order ===")
+    print(f"Cone: {chosen_cone.title()}")
+
     for i in range(num_scoops):
         print(f"Scoop {i+1}: {chosen_flavors[i].title()}")
                     #Makes it code start counting at 1
     if chosen_toppings:
         print("\nToppings:")
-        #loop through the tops
-        for toppings in chosen_toppings:
-            print(f" - {toppings.title()}")
+        #loop through the toppings
+        for topping in chosen_toppings:
+            print(f" - {topping.title()}")
+
+    scoop_cost, toppings_cost, total, discounted = calculate_total(num_scoops, len(chosen_toppings))        
     #print the total
-    total = calculate_total(num_scoops, len(chosen_toppings))
+
+    #show the discount
+    print(f"\nScoops Cost: ${scoop_cost:.2f}")
+    print(f"Toppings Cost: ${toppings_cost:.2f}")
     print(f"\nTotal: ${total:.2f}")
+
+    if discounted:
+        print(f"Disounted Applied! New Total: ${total:.2f}")
+    
     #save order to file
     with open("daily_orders.txt", "a") as file:
-        file.write(f"/nOrder: {num_scoops} scoops -${total:.2f}")
+        file.write(f"/nOrder: {num_scoops} scoops, {chosen_cone} cone -${total:.2f}")
 
 
 #Main function - simple test function
 def main():
     display_menu()
-    #calls get flavor functipn, which retruns # of scoops
+    #ask if user wants to search for a flavor prior to ordering
+    search_choice = input("\nWould you like to search for a flavor before you order? (yes/no): ").lower()
+    if search_choice == "yes":
+        search_flavor()
+    
+    #calls get flavor function, which retruns # of scoops
     #and the list of flavors
     num_scoops, chosen_flavors = get_flavors()
     chosen_toppings= get_toppings()
+    chosen_cone = get_cone()
 #display recipt
-    print_receipt(num_scoops, chosen_flavors, chosen_toppings)
+    print_receipt(num_scoops, chosen_flavors, chosen_toppings, chosen_cone)
 
 #Automatically executes the main function when the app runs
 if __name__ == "__main__":
     main()
+
+"""Sorry for the late project"""
